7a87817b29d826904451dde3b82a490869982b1e
StellaaaHu <33272519+StellaaaHu@users.noreply.github.com>
2018-01-10 22:58:41 +0800

    lc_3_v2

--- leetcode_3.py
+++ leetcode_3.py
@@ -13,20 +13,26 @@ class Solution:
         root_str_list = list(root_str)
         sub_str = temp = ''
         if len(root_str_list) >= 2:
-        	sub_str = root_str_list[0]
-        	n = 0
-        	while n < len(root_str_list) - 1:
-        		if root_str_list[n + 1] != root_str_list[n] and root_str_list[n + 1] not in sub_str:
-        			sub_str += root_str_list[n + 1]
-        		else:
-        			if len(sub_str) > len(temp):
-        				temp = sub_str
-        			sub_str = root_str_list[n + 1]
-        		n += 1
-        elif len(root_str_list) == 1:
-        	temp = root_str_list[0]
+            sub_str = root_str_list[0]
+            n = 0
+            while n < len(root_str_list) - 1:
+                if root_str_list[n + 1] not in sub_str:
+                    sub_str += root_str_list[n + 1]
+                else:
+                    if len(sub_str) > len(temp):
+                        temp = sub_str
+                        split = sub_str.find(root_str_list[n + 1])
+                        sub_str = sub_str[(split + 1):]
+                        sub_str += root_str_list[n + 1]
+                    else:
+                        split = sub_str.find(root_str_list[n + 1])
+                        sub_str = sub_str[(split + 1):]
+                        sub_str += root_str_list[n + 1]
 
-        if len(sub_str) > len(temp):
-        	return len(sub_str)
-        else:
-        	return len(temp)
+                n += 1
+            if len(sub_str) > len(temp):
+                temp = sub_str
+        elif len(root_str_list) == 1:
+            temp = root_str_list[0]
+        
+        return len(temp)
