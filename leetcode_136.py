87e5d0af516f9cc5a13a6a1535036d9306ba476e
StellaaaHu <33272519+StellaaaHu@users.noreply.github.com>
2018-01-17 10:36:42 +0800

    lc_136_v1
    succeed but not good

--- leetcode_136.py
+++ leetcode_136.py
@@ -7,4 +7,19 @@ class Solution:
         :type nums: List[int]
         :rtype: int
         """
-        
+        listed_arr = sorted(nums)
+        if len(listed_arr) <= 1:
+        	result = listed_arr[0]
+        else:
+        	if listed_arr[0] != listed_arr[1]:
+        		result = listed_arr[0]
+        	elif listed_arr[-1] != listed_arr[-2]:
+        		result = listed_arr[-1]
+        	else:
+        		n = 1
+        		while n < len(listed_arr) - 1:
+        			if listed_arr[n] != listed_arr[n - 1]:
+        				if listed_arr[n + 1] != listed_arr[n]:
+        					result = listed_arr[n]
+        			n += 1
+        return result
