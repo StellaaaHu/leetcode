ab08585b6145ed9b8c67b62aae10d19e9f12f72b
StellaaaHu <33272519+StellaaaHu@users.noreply.github.com>
2018-02-27 16:25:14 +0800

    lc_345_v1
    succeed

--- leetcode_345.py
+++ leetcode_345.py
@@ -11,22 +11,21 @@ class Solution:
         :type s: str
         :rtype: str
         """
-        vowels_arr = ['a', 'e', 'i', 'o', 'u']
-        sort_out_arr = sorted_vowels = []
+        vowels_arr = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+        sort_out_arr = []
+        new_str = ''
         for i in range(0, len(s)):
         	if s[i] in vowels_arr:
         		sort_out_arr.append(s[i])
         if len(sort_out_arr) > 0:
-        	n = -1
-        	while -n <= len(sort_out_arr):
-        		sorted_vowels.append(sort_out_arr[n])
-        		n -= 1
         	m = 0
         	while m < len(s):
-        		if s[m] in vowels_arr:
-        			left_s = s[:m]
-        			right_s = s[(m + 1):]
-        			s = left_s + sorted_vowels[0] + right_s
-        			del sorted_vowels[0]
+        		if s[m] not in vowels_arr:
+        			new_str += s[m]
+        		else:
+        			new_str += sort_out_arr[-1]
+        			del sort_out_arr[-1]
         		m += 1
-        return s
+        	return new_str
+        else:
+        	return s
